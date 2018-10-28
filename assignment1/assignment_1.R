37ffc811bfd5298c97c57a9f565bcd9e26dd5129
christoph <christoph.kirchner@student.uibk.ac.at>
2018-10-28 23:47:26 +0100

    added lambda and more general solution

--- assignment1/assignment_1.R
+++ assignment1/assignment_1.R
@@ -1,4 +1,6 @@
 len = 30;
+lambda = 1;
+order = 5;
 x <- vector("numeric", len);
 y <- vector("numeric", len);
 
@@ -15,30 +17,26 @@ plot(x,y);
 z <- seq(0,1,0.01)
 lines(z, fun(z), col="green", type="l")
 
-phi_0 <- function(x){1}
-phi_1 <- function(x){x}
-phi_2 <- function(x){x^2}
-phi_3 <- function(x){x^3}
-phi_4 <- function(x){x^4}
+basisFunction <- function(x, i){x^i}
 
-vector_phi_0 <- vector("numeric", len)
-vector_phi_1 <- vector("numeric", len)
-vector_phi_2 <- vector("numeric", len)
-vector_phi_3 <- vector("numeric", len)
-vector_phi_4 <- vector("numeric", len)
+Phi <- matrix(0, len, order);
 
 for(i in 1:len){
-  vector_phi_0[i] <- phi_0(x[i]);
-  vector_phi_1[i] <- phi_1(x[i]);
-  vector_phi_2[i] <- phi_2(x[i]);
-  vector_phi_3[i] <- phi_3(x[i]);
-  vector_phi_4[i] <- phi_4(x[i]);
+  for(j in 0:(order-1)){
+    Phi[i,j+1] <- basisFunction(x[i], j);
+  }
 }
 
-Phi <- matrix(c(vector_phi_0, vector_phi_1, vector_phi_2, vector_phi_3, vector_phi_4), nrow=30, ncol=5)
 Phi_T <- t(Phi)
-Phi_t <- solve((Phi_T%*%Phi))%*%Phi_T
+Phi_reg <- Phi_T%*%Phi #+ lambda*diag(order)
+Phi_t <- solve(Phi_reg)%*%Phi_T
 w <- Phi_t%*%y
 
-reg <- function(x){w[5]*x^4 + w[4]*x^3 + w[3]*x^2 + w[2]*x + w[1]}
+reg <- function(x){
+  y = 0;
+  for(i in 0:(order-1)){
+    y = y + w[i+1]*x^i;
+  }
+  return(y)
+}
 lines(z, reg(z), col="red", type="l")
