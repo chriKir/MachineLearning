76d3f52f918eb125b400e272ff1f007cd629c76e
christoph <christoph.kirchner@student.uibk.ac.at>
2018-10-29 16:40:13 +0100

    better values

--- assignment1/assignment_1.R
+++ assignment1/assignment_1.R
@@ -1,20 +1,21 @@
-len = 30;
-lambda = 1;
-order = 5;
+len = 15;
+lambda =7;
+order = 9;
 x <- vector("numeric", len);
 y <- vector("numeric", len);
 
-fun <- function(x){2.5*x^4 - 2*x^3 + x^2 - 2*x};
+#fun <- function(x){3*x^5 + 2*x^4 - 2*x^3 + x^2 + 2*x};
+fun <- function(x){ 0.5*x^4 + 4*x^3 - 5*x^2 + 10};
 
 for(i in 1:len){
   ran <- dnorm(i);
-  ranNr <- sample(0:10, 1)/10;
+  ranNr <- runif(1, -5.0, 5.0);
   x[i] <- ranNr;
-  y[i] <- fun(ranNr) + rnorm(1)/20;
+  y[i] <- fun(ranNr) + rnorm(1)*20;
 }
 
 plot(x,y);
-z <- seq(0,1,0.01)
+z <- seq(-5,5,0.1)
 lines(z, fun(z), col="green", type="l")
 
 basisFunction <- function(x, i){x^i}
@@ -28,7 +29,7 @@ for(i in 1:len){
 }
 
 Phi_T <- t(Phi)
-Phi_reg <- Phi_T%*%Phi #+ lambda*diag(order)
+Phi_reg <- Phi_T%*%Phi + lambda*diag(order)
 Phi_t <- solve(Phi_reg)%*%Phi_T
 w <- Phi_t%*%y
 
@@ -40,3 +41,16 @@ reg <- function(x){
   return(y)
 }
 lines(z, reg(z), col="red", type="l")
+
+errorFunction <- function(w){
+  result = 0;
+  for(n in 1:len){
+    result = result + (reg(x[n]) - fun(x[n]))^2 + lambda/2 * (t(w)*w)
+  }
+  return(result)
+}
+
+for(i in 1:(len/3)){
+  trainingValues = vector("numeric", len-3);
+  
+}
